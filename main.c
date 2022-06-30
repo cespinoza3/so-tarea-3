1faaec80715f1ef48778bd72d75eb638856fb5e4
cespinoza3 <103214138+cespinoza3@users.noreply.github.com>
2022-06-30 12:37:24 -0400

    agregar base para trabajo

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+typedef struct Cajero {
+    float demora_min;
+    float demora_max;
+} Cajero;
+
+typedef struct Cliente {
+    float demora_min;
+    float demora_max;
+} Cliente;
+
+int main(int argc, char* argv[]) {
+
+    return 0;
+}
