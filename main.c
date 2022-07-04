1a498a8d9e988cba9482d5f189bde6738abf7838
cespinoza3 <103214138+cespinoza3@users.noreply.github.com>
2022-07-04 12:55:28 -0400

    agregar base de codigo

--- main.c
+++ main.c
@@ -1,16 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <semaphore.h>
 
-typedef struct Cajero {
-    float demora_min;
-    float demora_max;
-} Cajero;
+#define MAX_CLIENTS 10
+#define MIN_PRODUCTS 5
+#define MAX_PRODUCTS 15
 
-typedef struct Cliente {
-    float demora_min;
-    float demora_max;
-} Cliente;
+typedef struct IIPair {
+    int first;
+    int second;
+} IIPair;
+
+typedef struct Client {
+    IIPair delay_range;
+
+    sem_t can_produce;
+} Client;
+
+typedef struct CashRegister {
+    int capacity;
+    IIPair delay_range;
+
+    int client_count;
+    Client clients[MAX_CLIENTS];
+    int current_client;
+
+    int conveyer_belt_capacity;
+    int conveyer_belt_count;
+    int conveyer_belt[MAX_PRODUCTS];
+
+    sem_t can_consume;
+} CashRegister;
+
+int random_between(int lower, int upper) {
+    return lower + random() % (upper - lower);
+}
 
 int main(int argc, char* argv[]) {
 
     return 0;
 }
+
+
