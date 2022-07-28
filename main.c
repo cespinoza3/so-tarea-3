c800f98562680d990928c9aca6e675d4ffd619e8
Cespinoza <103214138+cespinoza3@users.noreply.github.com>
2022-07-28 19:06:54 -0400

    agregar parsing de argumentos de programa

--- main.c
+++ main.c
@@ -62,9 +62,70 @@ int random_between_iipair(IIPair pair) {
 }
 
 
-int main(int argc, char* argv[]) {
+typedef struct CliArgs CliArgs;
+struct CliArgs {
+    int number_registers;
+    int number_clients;
+    int max_clients_per_register;
+    int max_products_per_client;
+};
+CliArgs parse_argv(int, char*[]);
 
+int main(int argc, char* argv[]) {
+    CliArgs args = parse_argv(argc, argv);
     return 0;
+}
+
+
+static const char* usage_string = ""
+"so-tarea-3 [ <numero cajas> ] [ <numero clientes> ] [ <max clientes por caja> ] [ <max productos por cliente> ]"
+"\n" "\n"
+"Opciones:"
+"\n"
+"  <numero cajas>               -  N° de cajas en la simulación [por defecto: 2]"
+"\n"
+"  <numero clientes>            -  N° de clientes en la simulación [por defecto: 20]"
+"\n"
+"  <max clientes por caja>      -  N° máximo de clientes que se atenderán por caja [por defecto: 10]"
+"\n"
+"  <max productos por cliente>  -  N° máximo de productos que tendrá un cliente [por defecto: 20]"
+"\n"
+"";
+
+CliArgs parse_argv(int argc, char* argv[]) {
+    CliArgs args = {
+        2,
+        20,
+        10,
+        20
+    };
+
+    switch (argc) {
+        case 5:
+            args.max_products_per_client = atoi(argv[4]);
+        case 4:
+            args.max_clients_per_register = atoi(argv[3]);
+        case 3:
+            args.number_clients = atoi(argv[2]);
+        case 2:
+            args.number_registers = atoi(argv[1]);
+        case 1:
+            break;
+        default:
+            myreport(-1, usage_string);
+    }
+
+    if (args.number_clients == 0 || args.number_registers == 0 || args.max_clients_per_register == 0 || args.max_products_per_client == 0) {
+        myreport(-1, ""
+        "error en valores dados:\n"
+        "numero cajas: %d\n"
+        "numero clientes: %d\n"
+        "max clientes por caja: %d\n"
+        "max productos por cliente: %d\n"
+        "", args.number_registers, args.number_clients, args.max_clients_per_register, args.max_products_per_client);
+    }
+
+    return args;
 }
 
 
