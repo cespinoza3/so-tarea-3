ee4d0a153d00ffd813667c84fada110ae40a0506
Cespinoza <103214138+cespinoza3@users.noreply.github.com>
2022-07-28 17:45:43 -0400

    agregar funciones para escribir a la pantalla

--- main.c
+++ main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 #include <semaphore.h>
+
+void my_vfprintf(FILE* f, const char* fmt, va_list args) {
+    fprintf(f, "so-tarea-3: ");
+    vfprintf(f, fmt, args);
+}
+
+void my_fprintf(FILE* f, const char* fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    my_vfprintf(f, fmt, args);
+    va_end(args);
+}
+
+void myreport(int exit_code, const char* fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    my_vfprintf(stderr, fmt, args);
+    va_end(args);
+    exit(exit_code);
+}
 
 #define MAX_CLIENTS 10
 #define MIN_PRODUCTS 5
